ab6ec2260e6824680a4cf175f231d66ac719278e
Andrii Myrhorodskyi <mirgorodskijav@gmail.com>
2021-02-04 13:38:49 +0200

    Add validation method and BeforeCreate hook

--- pkg/models/student.go
+++ pkg/models/student.go
@@ -1,9 +1,25 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"gorm.io/gorm"
+)
 
 type Student struct {
 	gorm.Model
 	Firstname string
 	Lastname  string
+	Group     string
+	Course    uint
+}
+
+func (s *Student) IsValid() bool {
+	return s.Firstname != "" && s.Lastname != "" && s.Group != "" && s.Course != 0
+}
+
+func (s *Student) BeforeCreate(_ *gorm.DB) (err error) {
+	if !s.IsValid() {
+		err = errors.New("student's data is not valid")
+	}
+	return
 }
